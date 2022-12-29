af56d6d74ccaf389742427435dbff8d26c2941d8
Parthasarathy <parthaa235@gmail.com>
2022-12-29 18:12:48 +0530

    working

--- swistat.java
+++ swistat.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 class swistat{
     public static void main(String args[]){
         Scanner x=new Scanner(System.in);
-        System.out.println("enter train class")
+        System.out.println("enter train class");
         char y=x.next().charAt(0); // to read the given character with index 0
 
         switch(y){
             case "a":
-            System.out.println("you enter ac class")
+            System.out.println("you enter ac class");
             break;
             case "e":
-            System.out.println("you enter economy class")
+            System.out.println("you enter economy class");
             case "s":
-            System.out.println("you enter sleeper class")
+            System.out.println("you enter sleeper class");
             break;
             case "g":
-            System.out.println("you enter general class")
+            System.out.println("you enter general class");
             break;
             default:
-            System.out.println("your choice not valid")
+            System.out.println("your choice not valid");
             break;
         }
     }
